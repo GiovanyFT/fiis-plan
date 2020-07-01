2f4f9d165fb31a7169ff88132b8d04a6d4f35559
Giovany Frossard Teixeira <giovanyfrossard@gmail.com>
2020-07-01 16:06:57 -0300

    Diferenciando menu lateral Android/iOS

--- lib/telas/localwidget/menu_lateral_admin.dart
+++ lib/telas/localwidget/menu_lateral_admin.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -10,7 +9,6 @@ import 'package:fundosimobiliarios/telas/tela_login.dart';
 import 'package:fundosimobiliarios/database_storage.dart';
 import 'package:fundosimobiliarios/util/gerenciadora_arquivo.dart';
 import 'package:fundosimobiliarios/util/nav.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 class MenuLateralAdmin extends StatelessWidget {
   Usuario usuario;
@@ -31,97 +29,121 @@ class MenuLateralAdmin extends StatelessWidget {
     return SafeArea(
       child: Drawer(
         child: ListView(
-          children: <Widget>[
-            FutureBuilder<Usuario>(
-              future: future,
-              builder: (context, snapshot) {
-                usuario = snapshot.data;
-                if (usuario == null){
-                  return Container();
-                }
-                else if (usuario.urlFoto != null){
-                  Future<File> future_arquivo = GerenciadoraArquivo.obterImagem(usuario.urlFoto);
-                  return FutureBuilder<File>(
-                      future: future_arquivo,
-                      builder: (context, snapshot) {
-                        if(!snapshot.hasData){
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-
-                        File imagem = snapshot.data;
-                        return _header(FileImage(imagem));
-                      }
-                  );
-                } else {
-                  return _header(AssetImage("assets/icon/icone_aplicacao.png"));
-                }
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.help),
-              title: Text("Ajuda"),
-              subtitle: Text("como usar"),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                // Fechando o menu lateral
-                pop(context);
-
-                push(context, TelaAjuda());
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.backup),
-              title: Text("Backup"),
-              subtitle: Text("salvando o banco de dados na nuvem"),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () async {
-                // Fechando o menu lateral
-                pop(context);
-
-                // Salvando o banco de dados na nuvem
-                DataBaseStorage.enviarBDParaStorage(usuario.login);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.restore),
-              title: Text("Restore"),
-              subtitle: Text("buscando o banco de dados na nuvem"),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () async {
-                // Fechando o menu lateral
-                pop(context);
-
-                // Salvando o banco de dados na nuvem
-                DataBaseStorage.buscarBDDoStorage(usuario.login);
-
-                // Sobrescrevendo a tela de Login
-                push(context, TelaLogin(), replace: true);
-
-                // Retirando o usuário das Shared Preferences
-                Usuario.limpar();
-              },
-            ),
-
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text("Sair"),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                // Fechando o menu lateral
-                pop(context);
-
-                // Sobrescrevendo a tela de Login
-                push(context, TelaLogin(), replace: true);
-
-                // Retirando o usuário das Shared Preferences
-                Usuario.limpar();
-              },
-            )
-          ],
+          children: Platform.isAndroid
+              ? <Widget>[
+                  _cabecalho(future),
+                  _ajuda(context),
+                  _backup(context),
+                  _restore(context),
+                  _sair(context)
+                ]
+              : <Widget>[
+                  _cabecalho(future),
+                  _ajuda(context),
+                  _sair(context),
+                ],
         ),
       ),
+    );
+  }
+
+  ListTile _sair(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.exit_to_app),
+      title: Text("Sair"),
+      trailing: Icon(Icons.arrow_forward),
+      onTap: () {
+        // Fechando o menu lateral
+        pop(context);
+
+        // Sobrescrevendo a tela de Login
+        push(context, TelaLogin(), replace: true);
+
+        // Retirando o usuário das Shared Preferences
+        Usuario.limpar();
+      },
+    );
+  }
+
+  ListTile _restore(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.restore),
+      title: Text("Restore"),
+      subtitle: Text("buscando o banco de dados na nuvem"),
+      trailing: Icon(Icons.arrow_forward),
+      onTap: () async {
+        // Fechando o menu lateral
+        pop(context);
+
+        // Salvando o banco de dados na nuvem
+        DataBaseStorage.buscarBDDoStorage(usuario.login);
+
+        // Sobrescrevendo a tela de Login
+        push(context, TelaLogin(), replace: true);
+
+        // Retirando o usuário das Shared Preferences
+        Usuario.limpar();
+      },
+    );
+  }
+
+  ListTile _backup(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.backup),
+      title: Text("Backup"),
+      subtitle: Text("salvando o banco de dados na nuvem"),
+      trailing: Icon(Icons.arrow_forward),
+      onTap: () async {
+        // Fechando o menu lateral
+        pop(context);
+
+        // Salvando o banco de dados na nuvem
+        DataBaseStorage.enviarBDParaStorage(usuario.login);
+      },
+    );
+  }
+
+  ListTile _ajuda(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.help),
+      title: Text("Ajuda"),
+      subtitle: Text("como usar"),
+      trailing: Icon(Icons.arrow_forward),
+      onTap: () {
+        // Fechando o menu lateral
+        pop(context);
+
+        push(context, TelaAjuda());
+      },
+    );
+  }
+
+  FutureBuilder<Usuario> _cabecalho(Future<Usuario> future) {
+    return FutureBuilder<Usuario>(
+      future: future,
+      builder: (context, snapshot) {
+        usuario = snapshot.data;
+        if (usuario == null) {
+          return Container();
+        } else if (usuario.urlFoto != null) {
+          Future<File> future_arquivo =
+              GerenciadoraArquivo.obterImagem(usuario.urlFoto);
+          return FutureBuilder<File>(
+              future: future_arquivo,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                File imagem = snapshot.data;
+                return _header(FileImage(imagem));
+              });
+        } else {
+          return _header(AssetImage("assets/icon/icone_aplicacao.png"));
+        }
+      },
     );
   }
 }
