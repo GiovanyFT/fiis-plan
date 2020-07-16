c074b6f3d5b94b06645beda31b98529259d7a92c
GiovanyFT <giovanyfrossard@gmail.com>
2020-07-16 09:54:44 -0300

    Limpeza de código

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:fundosimobiliarios/telas/tela_abertura.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,  // Add global cupertino localiztions.
+        GlobalCupertinoLocalizations.delegate,
       ],
+      // Força português independente o idioma do smartphone
+      // Deixei essa linha apenas para mostrar, num aplicativo a ser
+      // disponibilizado no AppStore, ela deve ser retirada
       locale: Locale('pt', 'BR'),  // Current locale
       supportedLocales: [
         const Locale('en', 'US'), // English
-        const Locale('th', 'TH'), // Thai
         const Locale('pt', 'BR'), // Brasil
       ],
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
